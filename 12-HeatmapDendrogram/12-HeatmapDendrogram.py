299ee3e8753fb6aff20596f9a6c71ea649b9b601
hoklavat <barisyildiz1982@gmail.com>
2020-10-20 21:39:15 +0300

    .

--- 12-HeatmapDendrogram/12-HeatmapDendrogram.py
+++ 12-HeatmapDendrogram/12-HeatmapDendrogram.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
+#12-HeatmapDendrogram
 
 import numpy as np
 import pandas as pd
@@ -10,28 +6,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
-
-# In[2]:
-
-
 data = pd.read_csv('CountryClustersStandardized.csv', index_col='Country')
-
-
-# In[3]:
-
 
 x_scaled = data.copy()
 x_scaled = x_scaled.drop(['Language'], axis=1)
 
-
-# In[4]:
-
-
 x_scaled
 
-
-# In[5]:
-
-
 sns.clustermap(x_scaled, cmap='mako')
-
